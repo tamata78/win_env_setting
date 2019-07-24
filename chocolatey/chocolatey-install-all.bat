dfa9a52039504c565a8374a4e30b2990c5780d01
tamata78 <5101956+tamata78@users.noreply.github.com>
2019-07-24 13:36:31 +0900

    add gitbash,curl to inst list

--- chocolatey/chocolatey-install-all.bat
+++ chocolatey/chocolatey-install-all.bat
@@ -1,5 +1,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;C:\chocolatey\bin"
 
 call cinst .\packages.config
+call cinst git -params '"/GitAndUnixToolsOnPath"'
 
 pause
