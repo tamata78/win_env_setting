015a47a7d95fba3819fb7ec094e48f3c66670a09
tamata78 <5101956+tamata78@users.noreply.github.com>
2019-07-27 14:13:17 +0900

    modify win_close_shell

--- close/close_dev_env.sh
+++ close/close_dev_env.sh
@@ -1,2 +1,10 @@
+@rem unistall chocolatey
+rmdir /s /q C:\ProgramData\chocolatey @rem del app_dir
+
+@rem del env variable for choco
+reg delete HKCU\Environment /v ChocolateyLastPathUpdate
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v ChocolateyInstall
+
 @rem del workDir with confirmation
-rd /s C:\Users\%username%\work
+rmdir /s C:\Users\%username%\work
+
